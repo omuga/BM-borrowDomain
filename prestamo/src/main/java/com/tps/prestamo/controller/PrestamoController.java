62e81e344fc4068c1e85db45bb0a6b7ef62ec4fb
omuga <Obriel.muga.14@sansano.usm.cl>
2019-08-17 18:32:31 -0400

    Se agregan pruebas unitarias para los controladores

--- prestamo/src/main/java/com/tps/prestamo/controller/PrestamoController.java
+++ prestamo/src/main/java/com/tps/prestamo/controller/PrestamoController.java
@@ -4,32 +4,33 @@ import com.tps.prestamo.repository.PrestamoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.*;
 import com.tps.prestamo.models.Prestamo; 
-import java.sql.Date;
 
 
 @Controller
 @RequestMapping(path = "/borrow")
 public class PrestamoController {
+
     @Autowired
     private PrestamoRepository prestamoRepository;
 
-    @GetMapping(path= "/add")
-    public @ResponseBody String addNewPrestamo(@RequestParam Long id_solicitante,
-    @RequestParam Long id_prestamista,@RequestParam Long id_bookitem,  @RequestParam Date fecha_inicio, @RequestParam Date fecha_termino){
-        if (id_prestamista.equals(id_solicitante)){
-            return "Error, id solicitante e id de prestamista son identicas";
+    public PrestamoController(PrestamoRepository prestamoRepository){
+        this.prestamoRepository = prestamoRepository;
+    }
+
+    @PostMapping(path= "/prestamo")
+    public @ResponseBody String addNewPrestamo(@RequestBody Prestamo prestamo){
+        Long id_prest = prestamo.getIdPrestamista();
+        Long id_sol = prestamo.getIdSolicitante();
+        if (id_prest.equals(id_sol)){
+            return "ID de solicitante y prestamista son iguales";
         }
-        Prestamo prestamo = new Prestamo();
-        prestamo.setIdPrestamista(id_prestamista);
-        prestamo.setIdSolicitante(id_solicitante);
-        prestamo.setIdBookItem(id_bookitem);
-        prestamo.setFechaInicio(fecha_inicio);
-        prestamo.setFechaTermino(fecha_termino);
         prestamoRepository.save(prestamo);
         return "Prestamo Almacenado";
     }
--- prestamo/src/test/java/com/tps/prestamo/DemoApplicationTests.java
+++ prestamo/src/test/java/com/tps/prestamo/DemoApplicationTests.java
@@ -1,14 +1,86 @@
 package com.tps.prestamo;
 
+import com.tps.prestamo.controller.PrestamoController;
+import com.tps.prestamo.models.Prestamo;
+import com.tps.prestamo.repository.PrestamoRepository;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.junit.Before;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.sql.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+	protected MockMvc mockMvc;
 
+	protected String mapToJson(Object obj) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(obj);
+	}
+
+	protected <T> T mapFromJson(String json, Class<T> clazz)
+		throws JsonParseException, JsonMappingException, IOException {
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.readValue(json, clazz);
+	}
+	@Mock
+	PrestamoRepository prestamoRepository;
+
+	@Before
+	public void setUp(){
+
+		final PrestamoController prestamoController = new PrestamoController(prestamoRepository);
+		mockMvc = MockMvcBuilders.standaloneSetup(prestamoController).build();
+	}
+
+	@Test
+	public void prestamoControllerPostTest() throws Exception{
+		String str="2015-03-31";  
+		String str2="2015-04-31";  
+		Date date_inicio = Date.valueOf(str);
+		Date date_final = Date.valueOf(str2);
+		String uri = "/borrow/prestamo";
+		Prestamo prestamo = new Prestamo();
+		Long id_sol = (long) 1;
+		Long id_pres = (long) 2;
+		Long id_book = (long) 1;
+		prestamo.setIdPrestamista(id_pres);
+		prestamo.setIdSolicitante(id_sol);
+		prestamo.setFechaInicio(date_inicio);
+		prestamo.setFechaTermino(date_final);
+		prestamo.setIdBookItem(id_book);
+		String inputJson = mapToJson(prestamo);
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post(uri)
+	  .contentType(org.springframework.http.MediaType.APPLICATION_JSON).content(inputJson)).andReturn();
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+	}
+
+	@Test
+	public void PrestamoControllerGetAllTest() throws Exception {
+		String uri = "/borrow/all";
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(uri)
+	  .contentType(org.springframework.http.MediaType.APPLICATION_JSON)).andReturn();
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+	}
 	@Test
 	public void contextLoads() {
 	}
